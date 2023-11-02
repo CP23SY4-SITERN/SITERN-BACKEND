83148e32c2824ae129e252dd10cb1c551a5514ae
sSilence <sarin.kobfha@mail.kmutt.ac.th>
2023-11-02 16:37:35 +0700

    init blank project

---
+++ sitern-backend/src/main/java/com/example/siternbackend/SiternBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.siternbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiternBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiternBackendApplication.class, args);
+    }
+
+}
---
+++ sitern-backend/src/test/java/com/example/siternbackend/SiternBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.siternbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiternBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
