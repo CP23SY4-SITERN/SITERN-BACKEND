f2b516bfdf32140f51cf6a050c749850d28b34bf
sSilence <sarin.kobfha@mail.kmutt.ac.th>
2023-11-16 23:11:33 +0700

    add JobService and add getJobById

--- sitern-backend/src/main/java/com/example/siternbackend/jobs/controllers/JobController.java
+++ sitern-backend/src/main/java/com/example/siternbackend/jobs/controllers/JobController.java
@@ -1,25 +1,69 @@
 package com.example.siternbackend.jobs.controllers;
 
 import com.example.siternbackend.jobs.entities.JobPost;
-import com.example.siternbackend.jobs.repositories.JobPostRepository;
+import com.example.siternbackend.jobs.entities.JobPostId;
+import com.example.siternbackend.jobs.services.JobService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/jobs")
 public class JobController {
 
-    private JobPostRepository jobPostRepository;
+    private final JobService jobService;
 
-    public JobController(JobPostRepository jobPostRepository) {
-        this.jobPostRepository = jobPostRepository;
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
     }
 
     @GetMapping
-    public List<JobPost> getAllJobs(){
-        return jobPostRepository.findAll();
+    public ResponseEntity<List<JobPost>> getAllJobs(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id,asc") String[] sort) {
+        try {
+            Pageable pageable = PageRequest.of(page, size, Sort.by(sort));
+            Page<JobPost> jobPage = jobService.getAllJobs(pageable);
+
+            List<JobPost> jobs = jobPage.getContent();
+
+            if (jobs.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(jobs, HttpStatus.OK);
+        } catch (Exception e) {
+            // Handle exceptions, log the error, and return an appropriate response
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<JobPost> getJobById(@PathVariable JobPostId id) {
+        try {
+            Optional<JobPost> jobPostOptional = jobService.getJobById(id);
+
+            if (jobPostOptional.isPresent()) {
+                return new ResponseEntity<>(jobPostOptional.get(), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            // Handle exceptions, log the error, and return an appropriate response
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
---
+++ sitern-backend/src/main/java/com/example/siternbackend/jobs/services/JobService.java
@@ -0,0 +1,34 @@
+package com.example.siternbackend.jobs.services;
+
+import com.example.siternbackend.jobs.entities.JobPost;
+import com.example.siternbackend.jobs.entities.JobPostId;
+import com.example.siternbackend.jobs.repositories.JobPostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JobService {
+    private final JobPostRepository jobPostRepository;
+
+    @Autowired
+    public JobService(JobPostRepository jobPostRepository) {
+        this.jobPostRepository = jobPostRepository;
+    }
+
+    public List<JobPost> getAllJobs() {
+        return jobPostRepository.findAll();
+    }
+
+    public Page<JobPost> getAllJobs(Pageable pageable) {
+        return jobPostRepository.findAll(pageable);
+    }
+
+    public Optional<JobPost> getJobById(JobPostId id) {
+        return jobPostRepository.findById(id);
+    }
+}
