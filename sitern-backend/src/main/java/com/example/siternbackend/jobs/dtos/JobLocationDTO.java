8f589277acdd033d39073d027f61735a253e1fd0
jubbyr <udomchai44@icloud.com>
2023-12-10 19:10:35 +0700

    @validate creating job and job location

--- sitern-backend/src/main/java/com/example/siternbackend/jobs/dtos/JobLocationDTO.java
+++ sitern-backend/src/main/java/com/example/siternbackend/jobs/dtos/JobLocationDTO.java
@@ -1,5 +1,6 @@
 package com.example.siternbackend.jobs.dtos;
 
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,9 +13,15 @@ import lombok.Setter;
 public class JobLocationDTO {
 
     private Integer id;
+    @Size(max = 45 , message = "Road must lower than 45 characters")
     private String road;
+    @Size(max = 45 , message = "Sub District must lower than 45 characters")
     private String subDistrict;
+    @Size(max = 45 , message = "Province must lower than 45 characters")
     private String province;
+    @Size(max = 45 , message = "Country must lower than 45 characters")
+    //The longest country name in terms of characters is the United Kingdom of Great Britain and Northern Ireland. The name consists of 56 characters.
     private String country;
+    @Size(max = 45 , message = "Zip must lower than 45 characters")
     private String zip;
 }
